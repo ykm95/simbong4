0611e4cd3faa19475dfbd57d287dea9124e7010f
TofuNoh <shtmdwo94@naver.com>
2020-01-02 14:21:47 +0900

    user talent 추가 - 20/01/02 - TofuNoh

---
+++ src/main/java/page/controller/user/TalentController.java
@@ -0,0 +1,88 @@
+package page.controller.user;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import page.dto.Talent;
+import page.service.user.talent.TalentService;
+import page.util.PagingTalent;
+
+@Controller
+public class TalentController {
+	
+	  @Autowired private TalentService talentService;
+	
+	  private static final Logger logger= LoggerFactory.getLogger(TalentController.class);
+	
+	  @RequestMapping(value="/user/talent/list")
+	  public void talentList(Model model, PagingTalent paging, HttpServletRequest req) {
+		  
+		  logger.info(paging.toString());
+		  
+		  paging = talentService.getPaging(paging, req);
+		  
+		  List<Talent> talentlist = talentService.getTalentList(paging);
+		  
+		  int count = talentService.getCntTalent(paging);
+		  
+		  model.addAttribute("count", count);
+		  model.addAttribute("paging", paging);
+		  model.addAttribute("list", talentlist);
+			
+		  // OK!!!! 
+	  }
+	  
+	  
+	  @RequestMapping(value="/user/talent/view")
+	  public void talentView(int talentno, Model model) {
+		  
+		  Talent talent = talentService.getTalent(talentno);
+		  
+		  model.addAttribute("talent", talent);
+		  
+		  
+	  }
+	  
+	  @RequestMapping(value="/user/talent/write", method=RequestMethod.GET)
+	  public void talentWrite() {
+		 
+	  }
+	  
+	  @RequestMapping(value="/user/talent/write", method=RequestMethod.POST)
+	  public String talentWriteProc(Talent talent) {
+		  
+		  talentService.write(talent);
+		  
+		  return "redirect:/user/talent/list";
+	  }
+	  
+	  @RequestMapping(value="/user/talent/update", method=RequestMethod.GET)
+	  public void talentUpdate() {
+		  
+	  }
+	  
+	  @RequestMapping(value="/user/talent/update", method=RequestMethod.POST)
+	  public String talentUpdateProc(Talent talent) {
+		  
+		  talentService.update(talent);
+		  
+		  return "redirect:/user/talent/list";
+	  }
+	  
+	  @RequestMapping(value="/user/talent/delete")
+	  public String talentDelete(int talentno) {
+		  
+		  talentService.delete(talentno);
+		  
+		  return "redirect:/user/talent/list";
+	  }
+}
