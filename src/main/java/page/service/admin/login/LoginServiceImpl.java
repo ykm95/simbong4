bab25d426aea29f4c502a8df8f1f8b90f3281ca8
angela <yunsol96@naver.com>
2020-01-03 10:10:08 +0900

    serviceimpl 변경

--- src/main/java/page/service/admin/login/LoginServiceImpl.java
+++ src/main/java/page/service/admin/login/LoginServiceImpl.java
@@ -2,7 +2,15 @@ package page.service.admin.login;
 
 import org.springframework.stereotype.Service;
 
+import page.dto.Admin;
+
 @Service
 public class LoginServiceImpl implements LoginService{
+
+	@Override
+	public boolean login(Admin admin) {
+		
+		return false;
+	}
 
 }
