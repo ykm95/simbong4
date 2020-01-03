bb8434d2d618e876ca4648eae661b2c98b66b1c0
angela <yunsol96@naver.com>
2020-01-03 10:02:58 +0900

    에러 고침

--- src/main/java/page/controller/admin/login/AdminLoginController.java
+++ src/main/java/page/controller/admin/login/AdminLoginController.java
@@ -1,24 +1,56 @@
 package page.controller.admin.login;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import page.dto.Admin;
 
 
 @Controller
 public class AdminLoginController {
 	
+	@Autowired private page.service.admin.login.LoginService loginService;
+	
 	private static final Logger logger = LoggerFactory.getLogger(AdminLoginController.class);
 	
-	//메인페이지(비로그인 상태)
-		//DAO생략-@Autowired생략
-
-		@RequestMapping(value="/admin/adminLogin/login", method=RequestMethod.GET)
-		public void loginmain() {
+	@RequestMapping(value="admin/adminLogin/login", method= RequestMethod.GET)
+	public void login() {}
+	
+	@RequestMapping(value="/admin/adminLogin/login", method=RequestMethod.POST)
+	public String loginprocess(Admin admin, HttpSession session) {
+			
+	logger.info(admin.toString());
+			
+	//아이디, 패스워드 DB 조회
+	boolean isLogin = loginService.login(admin); //true-인증 성공
+			
+	//결과에 따른 세션처리
+	if(isLogin) {
+		session.setAttribute("login", isLogin);
+		session.setAttribute("loginid", admin.getAdminId());
+			}
+			
+			return "redirect:/admin/adminLogin/main";
 			
 		}
+		
+		
+		@RequestMapping(value="/admin/adminLogin/logout")
+		public String logout(HttpSession session) {
+			
+			session.invalidate();
+			
+			return "redirect:/admin/adminLogin/main";
+			
+		}
+	//메인페이지(비로그인 상태)
+		
 	//로그인(비로그인 → 로그인  / 로그인시 회원관리, 문의글관리, 게시물관리 세가지 버튼중 하나를 누르면 페이지로 이동)
 	
 	//로그아웃버튼과 동시에 메인페이지 리디렉션(로그인 →비로그인, 로그아웃되었습니다 모달창은 여유되면 만들기)
--- src/main/java/page/dto/Admin.java
+++ src/main/java/page/dto/Admin.java
@@ -2,34 +2,31 @@ package page.dto;
 
 public class Admin {
 	
-	private int adminno;		
-	private String admin_id;		
-	private String admin_pw;
+	private int adminNo;		
+	private String adminId;		
+	private String adminPw;
+	
+	public int getAdminNo() {
+		return adminNo;
+	}
+	public void setAdminNo(int adminNo) {
+		this.adminNo = adminNo;
+	}
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminPw() {
+		return adminPw;
+	}
+	public void setAdminPw(String adminPw) {
+		this.adminPw = adminPw;
+	}
 	
 	@Override
 	public String toString() {
-		return "Admin [adminno=" + adminno + ", admin_id=" + admin_id + ", admin_pw=" + admin_pw + "]";
+		return "Admin [adminNo=" + adminNo + ", adminId=" + adminId + ", adminPw=" + adminPw + "]";
 	}
-	
-	public int getAdminno() {
-		return adminno;
-	}
-	public void setAdminno(int adminno) {
-		this.adminno = adminno;
-	}
-	public String getAdmin_id() {
-		return admin_id;
-	}
-	public void setAdmin_id(String admin_id) {
-		this.admin_id = admin_id;
-	}
-	public String getAdmin_pw() {
-		return admin_pw;
-	}
-	public void setAdmin_pw(String admin_pw) {
-		this.admin_pw = admin_pw;
-	}
-	
-	
-	
 }
