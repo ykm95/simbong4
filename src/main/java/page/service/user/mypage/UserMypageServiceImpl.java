70adf347ccb24f9f5b360e2374c3849b00f5d3ec
정광주 <@118.34.220.172>
2020-01-12 16:10:11 +0900

    유저 마이페이지 1:1문의 작업완료

--- src/main/java/page/service/user/mypage/UserMypageServiceImpl.java
+++ src/main/java/page/service/user/mypage/UserMypageServiceImpl.java
@@ -1,14 +1,29 @@
 package page.service.user.mypage;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import page.dao.user.mypage.UserMypageDao;
 import page.dto.User;
+import page.service.center.mypage.CenterMypageServiceImpl;
+import page.dto.Question;
 
 @Service
 public class UserMypageServiceImpl implements UserMypageService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserMypageServiceImpl.class);
 
+	@Autowired ServletContext context;
+	
 	@Autowired UserMypageDao userMypageDao;
 	
 	@Override
@@ -43,6 +58,58 @@ public class UserMypageServiceImpl implements UserMypageService {
 	@Override
 	public void googleInformationUpdate(User user) {
 		userMypageDao.googleUpdate(user);
+	}
+
+	@Override
+	public List<Question> getList() {
+		return userMypageDao.selectAll();
+	}
+
+	@Override
+	public int getQuestionno() {
+		return userMypageDao.selectQuestionnoByDual();
+	}
+
+	@Override
+	public int getUserno(User user) {
+		return userMypageDao.selectUsernoByUemail(user);
+	}
+
+	@Override
+	public void writeQST(Question question, MultipartFile file) {
+		logger.info(context.getRealPath("TEST"));
+		
+		//파일이 저장될 경로
+		String storedPath = context.getRealPath("upload");
+		
+		//저장될 파일의 이름 (원본명 + UUID)
+		String filename = question.getQuestionno()+"";
+		
+		//저장될 파일 객체
+		File dest = new File(storedPath, filename);
+		
+		try {
+			file.transferTo(dest); //실제 파일 저장
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		question.setPic("/upload/" + filename);
+		
+		userMypageDao.insertQuestion(question);
+		
+	}
+
+	@Override
+	public Question viewQST(int questionno) {
+		return userMypageDao.selectQuestionByQuestionno(questionno);
+	}
+
+	@Override
+	public void deleteQST(Question question) {
+		userMypageDao.deleteQuestion(question);
 	}
 
 
---
+++ src/main/webapp/WEB-INF/views/user/mypage/questionForm.jsp
@@ -0,0 +1,76 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+
+<!-- 부트스트랩 3.3.2 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<!-- jQuery 2.2.4 -->
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+<!-- CK Editor -->
+<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	
+	//작성버튼 동작
+	$("#btnWrite").click(function() {
+		$("form").submit();
+	});
+	
+	//취소버튼 동작
+	$("#btnCancel").click(function() {
+		$(location).attr("href", "/user/mypage/mypagemain");
+	});
+});
+</script>
+
+</head>
+<body>
+
+<div class="container">
+
+<h3>유저회원문의</h3>
+<hr>
+
+<div><!-- 1 -->
+
+<form action="/user/mypage/writequestion" method="post" enctype="multipart/form-data">
+
+<table class="table table-bordered">
+<tr><td class="info">제목</td><td><input type="text" name="question_title" style="width:100%"/></td></tr>
+<tr><td class="info" colspan="2">본문</td></tr>
+
+<!-- CK Editor 적용영역 -->
+
+<tr><td colspan="2" ><textarea id="question_content" name="question_content"></textarea></td></tr>
+<script>
+	CKEDITOR.replace( 'question_content' );
+</script>
+
+<!-- CK Editor 적용영역 -->
+
+<tr><td class="info">첨부파일</td><td><input type="file" name="file" /></td></tr>
+</table>
+
+
+</form>
+
+</div><!-- 1 -->
+
+<div class="text-center">	
+	<button type="button" id="btnWrite" class="btn btn-info">작성</button>
+	<button type="button" id="btnCancel" class="btn btn-danger">취소</button>
+</div>
+
+</div> <!-- container -->
+
+</body>
+</html>
