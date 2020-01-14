d1e3e1202c4727f93f71765f9e9bc9dab444197d
ykm95 <ykm4045@gmail.com>
2020-01-14 23:00:54 +0900

    센터페이지 봉사활동 인증 프로세스 추가 - 01/14 기문

--- src/main/java/page/service/center/record/RecordService.java
+++ src/main/java/page/service/center/record/RecordService.java
@@ -2,6 +2,7 @@ package page.service.center.record;
 
 import java.util.List;
 
+import page.dto.Applicant;
 import page.util.Paging;
 
 public interface RecordService {
@@ -9,5 +10,9 @@ public interface RecordService {
 	public Paging getPaging(Paging paging, int centerno);
 
 	public List getRecordList(Paging paging);
+
+	public List<Applicant> getAplByNo(int volunteerno);
+
+	public void record(int volunteerno, int applicantno);
 
 }
